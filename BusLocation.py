119c80112528d1b8fdc4baf8eb1a71434fdf42a7
Amber Chen <amberchen@Ambers-MacBook-Pro.local>
2019-01-30 23:36:44 -0600

    Add bus stop coordinates

--- BusLocation.py
+++ BusLocation.py
@@ -23,3 +23,28 @@ for veh in vehs:
                   icon=folium.Icon(icon='bus',prefix='fa')).add_to(m)
 # Generate map
 m.save('map.html')
+
+# import stop location
+tripID = "2104620"
+filtered_stop_id = []
+with open("capmetro/stop_times.txt") as f:
+    for line in f:
+        (trip_id,arrival_time,departure_time,
+         stop_id,stop_sequence,stop_headsign,
+         pickup_type,drop_off_type,
+         shape_dist_traveled,timepoint) = line.split(",")
+        if trip_id==tripID:
+            filtered_stop_id.append(stop_id)
+            
+filtered_stop_lat = []
+filtered_stop_long = []
+with open("capmetro/stops.txt") as f:
+    for line in f:
+        (stop_id,stop_code,stop_name,stop_desc,
+         stop_lat,stop_lon,zone_id,stop_url,
+         location_type,parent_station,stop_timezone,
+         wheelchair_boarding,corner_placement,stop_position,
+         on_street,at_street,heading) = line.split(",")
+        if stop_id in filtered_stop_id:
+            filtered_stop_lat.append(float(stop_lat))
+            filtered_stop_long.append(float(stop_lon))
--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask
 import os
 import requests
 import json
@@ -27,7 +27,32 @@ def bus_locations(route_id):
     #     lat = veh['vehicle']['position']['latitude']
     #     long = veh['vehicle']['position']['longitude']
     #     routeID = veh['vehicle']['trip']['route_id']
-    return json.dumps(filteredVehs)
+    
+    # query stop ID using trip ID
+    filteredTripID = [veh['vehicle']['trip']['trip_id'] for veh in vehs if int(veh['vehicle']['trip']['route_id']) == route_id]
+    filtered_stop_id = []
+    with open("../capmetro/stop_times.txt") as f:
+        for line in f:
+            (trip_id,arrival_time,departure_time,
+             stop_id,stop_sequence,stop_headsign,
+             pickup_type,drop_off_type,
+             shape_dist_traveled,timepoint) = line.split(",")
+            if trip_id in filteredTripID:
+                filtered_stop_id.append(stop_id)
+    # query stop coordinates using stop ID            
+    filtered_stop_lat = []
+    filtered_stop_long = []
+    with open("../capmetro/stops.txt") as f:
+        for line in f:
+            (stop_id,stop_code,stop_name,stop_desc,
+             stop_lat,stop_lon,zone_id,stop_url,
+             location_type,parent_station,stop_timezone,
+             wheelchair_boarding,corner_placement,stop_position,
+             on_street,at_street,heading) = line.split(",")
+            if stop_id in filtered_stop_id:
+                filtered_stop_lat.append(float(stop_lat))
+                filtered_stop_long.append(float(stop_lon))
+    return json.dumps([filteredVehs,filtered_stop_lat,filtered_stop_long])
 
 
 if __name__ == '__main__':
@@ -35,3 +60,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
